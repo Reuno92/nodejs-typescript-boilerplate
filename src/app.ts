a147c988438a91106e330fa9da8893b2d17b67b4
Renaud RACINET <renauddev@MacBook-Pro-de-Renaud.local>
2020-12-02 11:28:51 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+
+export const getApp = (): Express => {
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  app.get('/api/v1/test', (_, res) => {
+    res.json({ ok: true });
+  });
+  return app;
+};
